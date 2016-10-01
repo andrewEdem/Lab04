484f873125f372478218cd520ef5d7e4bcae43ce
Andrew Edem <AndrewEdem@dhcp93-48.calvin.edu>
2016-09-30 21:30:43 -0400

    Initial commit

---
+++ app/src/main/java/edu/calvin/akg8/lab04/MainActivity.java
@@ -0,0 +1,42 @@
+package edu.calvin.akg8.lab04;
+
+/*
+* Lab04
+*
+* This simple app creates an About menu
+*
+* @author Andrew Gbeddy
+* @version fall 2016
+ */
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.about_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about:
+                startActivity(new Intent(getApplicationContext(), AboutActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
